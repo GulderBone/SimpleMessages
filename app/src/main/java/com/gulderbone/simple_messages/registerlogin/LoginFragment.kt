87f760b51d736915efa474b1946b69c0b51a97f9
Bartkowiak <mateuszbartkowiak00@gmail.com>
2021-12-05 21:00:21 +0100

    18 | Fix crashes from google play.

--- app/src/main/java/com/gulderbone/simple_messages/registerlogin/LoginFragment.kt
+++ app/src/main/java/com/gulderbone/simple_messages/registerlogin/LoginFragment.kt
@@ -5,16 +5,17 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.google.firebase.auth.FirebaseAuth
 import com.gulderbone.simple_messages.R
 import com.gulderbone.simple_messages.base.BaseFragment
-import com.gulderbone.simple_messages.main.MainViewModel
 import com.gulderbone.simple_messages.databinding.FragmentLoginBinding
 import com.gulderbone.simple_messages.extensions.TAG
 import com.gulderbone.simple_messages.extensions.visibleOrGone
+import com.gulderbone.simple_messages.main.MainViewModel
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>() {
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentLoginBinding =
@@ -33,16 +34,25 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
             val email = binding.emailEdittextLogin.editText?.text.toString()
             val password = binding.passwordEdittextLogin.editText?.text.toString()
 
+            if (email.isEmpty()) {
+                Toast.makeText(requireActivity(), "Please enter email", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if (password.isEmpty()) {
+                Toast.makeText(requireActivity(), "Please enter password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             Log.d(TAG, "Attempt login with email/pw: $email/***")
 
             baseViewModel.setLoaderVisibility(true)
 
             FirebaseAuth.getInstance()
                 .signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener {
+                .addOnSuccessListener {
                     navController.navigate(R.id.action_loginFragment_to_latestMessagesFragment)
                 }
                 .addOnFailureListener {
+                    Toast.makeText(requireActivity(), it.message, Toast.LENGTH_LONG).show()
                     Log.e(TAG, "Failed to log in: $it")
                 }
         }
