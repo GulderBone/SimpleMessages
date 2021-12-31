15cbe0985359f6be12cdda5905b2bfeb10d717f4
Bartkowiak <mateuszbartkowiak00@gmail.com>
2021-12-31 11:17:39 +0100

    Delete before gitignore

--- app/src/main/java/com/gulderbone/simple_messages/utils/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.gulderbone.simple_messages.utils
-
-object Constants {
-    const val BASE_URL = "https://fcm.googleapis.com"
-    const val SERVER_KEY = "AAAAr9WKQx0:APA91bEvtSWznMwNjuMXOTwAfje2y5KKs7SiK8j0s3YNY3evMO6qiVXdDGXU6a1XZSg9Fg4wSyTWPfEKiQaJHU8gHYaJhHjmyseLYF_znWMRZ2ZF_91RFMaK_kkLyPx9qLvYbEGZoRz5"
-    const val CONTENT_TYPE = "application/json"
-}
