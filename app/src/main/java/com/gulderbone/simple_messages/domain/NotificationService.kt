7a3a39941ab40a4e081ec5f108b14f3fef8b6b9d
Bartkowiak <mateuszbartkowiak00@gmail.com>
2022-01-03 19:21:11 +0100

    19 | Notifications hotfix

--- app/src/main/java/com/gulderbone/simple_messages/domain/NotificationService.kt
+++ app/src/main/java/com/gulderbone/simple_messages/domain/NotificationService.kt
@@ -8,6 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import com.google.firebase.auth.ktx.auth
@@ -15,8 +16,11 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.ktx.messaging
 import com.gulderbone.simple_messages.R
+import com.gulderbone.simple_messages.extensions.TAG
 import com.gulderbone.simple_messages.main.MainActivity
+import com.gulderbone.simple_messages.models.User
 import kotlin.random.Random
 
 private const val MESSAGE_CHANNEL_ID = "messages"
@@ -54,7 +58,20 @@ class NotificationService : FirebaseMessagingService() {
         val currentUserUid = Firebase.auth.uid ?: return
 
         val currentUserReference = Firebase.firestore.document("users/$currentUserUid")
-        currentUserReference.update("messagingToken", token)
+        currentUserReference.get().addOnSuccessListener { value ->
+            val currentUser = value?.toObject(User::class.java)
+            val messagingToken = currentUser?.messagingToken ?: return@addOnSuccessListener
+            Firebase.messaging.unsubscribeFromTopic(messagingToken)
+        }
+        currentUserReference.update("messagingToken", token).addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.e(TAG, "Token update failed: ", task.exception)
+                return@addOnCompleteListener
+            }
+            Log.d(TAG, "Token updated")
+        }
+
+        Firebase.messaging.subscribeToTopic(token)
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
